5760b47895e795e5b68dfc3b43a6fecd52a969eb
SainyTK <tanakorn0412@gmail.com>
2024-03-02 08:04:16 +0700

    Path lesson

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,16 +1,20 @@
 mod hello_world;
 mod mirror_hello;
 mod mirror_json;
+mod path_variables;
 
 use axum::{routing::{get, post}, Router};
 
 use hello_world::get_hello;
 use mirror_hello::mirror_hello;
 use mirror_json::mirror_json;
+use path_variables::{path_variables, hardcoded_path};
 
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", get(get_hello))
         .route("/mirror_hello", post(mirror_hello))
         .route("/mirror_json", post(mirror_json))
+        .route("/path_variables/:id", get(path_variables))
+        .route("/path_variables/15", get(hardcoded_path))
 }
